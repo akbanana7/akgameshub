49d14924e6b48fedec51af682f0dc1fa3662741c
akbanana7 <116113592+akbanana7@users.noreply.github.com>
2023-11-21 07:33:06 +0000

    Deleted Override page due to broken

--- Override/setcookies.js
+++
@@ -1,9 +0,0 @@
-// setCookie.js
-
-// Function to set a cookie that allows access
-function setAccessCookie() {
-    // Set an access cookie with a value (e.g., "access=true")
-    document.cookie = "access=true";
-    alert("Access granted! You have been granted access.");
-    // You can add your website content here
-}
