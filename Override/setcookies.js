6d2ba867dee4e9468c5bf3e75c81701b97e035dc
akbanana7 <116113592+akbanana7@users.noreply.github.com>
2023-11-23 15:57:15 +0000

    Added cookie expiry and updated games.html flex

--- Override/setcookies.js
+++ Override/setcookies.js
@@ -2,8 +2,16 @@
 
 // Function to set a cookie that allows access
 function setAccessCookie() {
-    // Set an access cookie with a value (e.g., "access=true")
-    document.cookie = "access=true; path=/; domain=" + window.location.hostname;
+    // Calculate the expiration date, one month from now
+    const expirationDate = new Date();
+    expirationDate.setMonth(expirationDate.getMonth() + 1);
+
+    // Format the expiration date in the UTC format (required by the cookie)
+    const formattedExpirationDate = expirationDate.toUTCString();
+
+    // Set an access cookie with a value (e.g., "access=true") and the expiration date
+    document.cookie = "access=true; path=/; domain=" + window.location.hostname + "; expires=" + formattedExpirationDate;
+
     alert("Access granted! You have been granted access.");
     // You can add your website content here
 }
