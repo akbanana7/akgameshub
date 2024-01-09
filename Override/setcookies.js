d15ed6b7c709cfe8636495a9d4e8ca4f1de8f51c
akbanana7 <116113592+akbanana7@users.noreply.github.com>
2024-01-09 17:38:18 +0000

    Now requires code to set cookie

--- Override/setcookies.js
+++ Override/setcookies.js
@@ -1,17 +1,15 @@
 // setCookie.js
 
-// Function to set a cookie that allows access
-function setAccessCookie() {
-    // Calculate the expiration date, one month from now
-    const expirationDate = new Date();
-    expirationDate.setMonth(expirationDate.getMonth() + 1);
+function checkAccessCode() {
+    const enteredCode = prompt("Enter the access code:");
 
-    // Format the expiration date in the UTC format (required by the cookie)
-    const formattedExpirationDate = expirationDate.toUTCString();
-
-    // Set an access cookie with a value (e.g., "access=true") and the expiration date
-    document.cookie = "access=true; path=/; domain=" + window.location.hostname + "; expires=" + formattedExpirationDate;
-
-    alert("Access granted! You have been granted access.");
-    // You can add your website content here
+    if (enteredCode === "6462") {
+        const expirationDate = new Date();
+        expirationDate.setMonth(expirationDate.getMonth() + 1);
+        const formattedExpirationDate = expirationDate.toUTCString();
+        document.cookie = "access=true; path=/; domain=" + window.location.hostname + "; expires=" + formattedExpirationDate;
+        alert("Access granted! You have been granted access.");
+    } else {
+        window.location.href = "https://www.google.com";
+    }
 }
