c7d82e3d35a6e7b34c8c0fbb2790c3bfc16d3c86
akbanana7 <116113592+akbanana7@users.noreply.github.com>
2024-01-09 18:13:55 +0000

    V0.03

--- Game/paperio2/cookies.js
+++ Game/paperio2/cookies.js
@@ -1,10 +1,15 @@
 // checkCookie.js
 
-// Check for the access cookie on page load
+// Check for the access and ban cookies on page load
 window.addEventListener("load", function () {
     const accessCookie = getCookie("access");
+    const banCookie = getCookie("ban");
 
-    if (!accessCookie) {
+    if (banCookie) {
+        // Ban cookie found, redirect to Google
+        alert("Access denied! You are banned.");
+        window.location.href = "https://www.google.com";
+    } else if (!accessCookie) {
         // No access cookie found, redirect to Google or another page
         alert("Access denied! You do not have access.");
         window.location.href = "https://www.google.com";
--- Override/setcookies.js
+++ Override/setcookies.js
@@ -5,11 +5,16 @@ function checkAccessCode() {
 
     if (enteredCode === "6462") {
         const expirationDate = new Date();
-        expirationDate.setMonth(expirationDate.getMonth() + 1);
+        expirationDate.setDate(expirationDate.getDate() + 1); // Set expiration to 1 day from now
         const formattedExpirationDate = expirationDate.toUTCString();
         document.cookie = "access=true; path=/; domain=" + window.location.hostname + "; expires=" + formattedExpirationDate;
         alert("Access granted! You have been granted access.");
     } else {
+        alert("Ha, get banned");
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 1); // Set expiration to 1 day from now
+        const formattedExpirationDate = expirationDate.toUTCString();
+        document.cookie = "ban=true; path=/; domain=" + window.location.hostname + "; expires=" + formattedExpirationDate;
         window.location.href = "https://www.google.com";
     }
 }
