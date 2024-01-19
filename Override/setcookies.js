feac80b857beda33a8510de8fe5a1339d64d027c
akbanana7 <116113592+akbanana7@users.noreply.github.com>
2024-01-19 15:49:43 +0000

    changed code due to comprimise

--- Override/setcookies.js
+++ Override/setcookies.js
@@ -3,7 +3,7 @@
 function checkAccessCode() {
     const enteredCode = prompt("Enter the access code:");
 
-    if (enteredCode === "6462") {
+    if (enteredCode === "33669900") {
         const expirationDate = new Date();
         expirationDate.setDate(expirationDate.getDate() + 1); // Set expiration to 1 day from now
         const formattedExpirationDate = expirationDate.toUTCString();
